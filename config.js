0090a77e03125f53b27cf8aef1a06a833a1776d3
MinhSangStudio <121650435+MinhSangStudio@users.noreply.github.com>
2022-12-30 14:10:25 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+const nameGirl = 'MinhSang';
+const giftUrl = 'https://www.facebook.com/minhsang.2k3.FA';
+const eventName = 'Chúc Mừng Sinh Nhật 1-10';
+const titleCard = 'Tặng  Minh Sang';
+const contentCard = 'Chúc bạn tôi tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với bạn trong hôm nay và cả những ngày sau';
+
+const giftImage = 'Minh Sang.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
